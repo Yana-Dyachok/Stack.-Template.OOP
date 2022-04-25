0b51673285c1ecb51fa2c432f147390553ef6651
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2022-04-25 22:45:33 +0300

    Add project files.

---
+++ Stack. Template.OOP/Stack. Template.OOP.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Stack.h"
+using namespace std;
+int main()
+{
+	Stack <int> A(3);//створюємо об'єкт A  з задданим розміром
+	cout<<"index < max =" << A.push(45) << endl;//якщо індекс менший максимального значення- заповнюємо стек значенням
+	cout << "index < max =" <<A.push(67) << endl;//якщо індекс менший максимального значення- заповнюємо стек значенням
+	cout << "index < max =" <<A.push(125) << endl;//якщо індекс менший максимального значення- заповнюємо стек значенням
+	cout << "Stack is full=" << A.isFull() << endl;//перевіряємо чи повний стек
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << A.pop() << "\t";//видаляємо елементи зі стеку
+	}
+	cout<<endl << "Stack is empty=" <<A.isEmpty() << endl;	//перевіряємо чи стек пустий
+}
